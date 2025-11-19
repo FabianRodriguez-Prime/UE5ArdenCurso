c6b024d1280437af1446dec9d3d0edee6a4eeb9c
FabianRodriguez-Prime <rodriguezfabian826@gmail.com>
2025-11-19 17:03:45 -0600

    002

---
+++ UE5Arden/Intermediate/Build/Win64/UnrealEditor/Inc/UE5Arden/UHT/UE5Arden.init.gen.cpp
@@ -0,0 +1,29 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/GeneratedCppIncludes.h"
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+void EmptyLinkFunctionForGeneratedCodeUE5Arden_init() {}
+	static FPackageRegistrationInfo Z_Registration_Info_UPackage__Script_UE5Arden;
+	FORCENOINLINE UPackage* Z_Construct_UPackage__Script_UE5Arden()
+	{
+		if (!Z_Registration_Info_UPackage__Script_UE5Arden.OuterSingleton)
+		{
+			static const UECodeGen_Private::FPackageParams PackageParams = {
+				"/Script/UE5Arden",
+				nullptr,
+				0,
+				PKG_CompiledIn | 0x00000000,
+				0x0FC5E198,
+				0x398CF4A3,
+				METADATA_PARAMS(0, nullptr)
+			};
+			UECodeGen_Private::ConstructUPackage(Z_Registration_Info_UPackage__Script_UE5Arden.OuterSingleton, PackageParams);
+		}
+		return Z_Registration_Info_UPackage__Script_UE5Arden.OuterSingleton;
+	}
+	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_UE5Arden(Z_Construct_UPackage__Script_UE5Arden, TEXT("/Script/UE5Arden"), Z_Registration_Info_UPackage__Script_UE5Arden, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0x0FC5E198, 0x398CF4A3));
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
